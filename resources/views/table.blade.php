527d33a9c80d7ec8e365ea4461f5614fbb0d906d
Roshan Bhusal <rosan_wanted@yahoo.com>
2024-09-17 23:00:14 +0100

    Getting started. This alpine stuff is annoying, trying to avoid it as much as possible.

---
+++ resources/views/table.blade.php
@@ -0,0 +1,17 @@
+<h1>helllllllllllll </h1>
+
+
+<table>
+
+    <ul>
+        @for ($i=0; $i <$daysInMonth; $i++)
+            <li> {{$i}}</li>
+        @endfor
+        <td> {{$daysInMonth}}   </td>
+
+</ul>
+
+
+
+</table>
+
