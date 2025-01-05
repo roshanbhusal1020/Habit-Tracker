6b9c2e8b9ce6d7cf1bf3799a9da66172a438db66
Roshan Bhusal <rosan_wanted@yahoo.com>
2025-01-05 14:45:06 +0000

    tested the chart, works fine. havent put any chart code yet but it works.

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class StatsController extends Controller
 {
-    //
+    public function getPomodoroStats() {
+
+    }
+
+    public function getHabitTableStats() {
+        
+    }
+
+    public function getTodoListStats() {
+        
+    }
 }
