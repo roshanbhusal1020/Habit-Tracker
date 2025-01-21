c2e3c0a9d2404a68882af378ee015eee78711ce4
Roshan Bhusal <rosan_wanted@yahoo.com>
2025-01-21 00:32:05 +0000

    final test for today

--- resources/views/stats/dashboard.blade.php
+++ resources/views/stats/dashboard.blade.php
@@ -1,2 +1,2 @@
-<h1> Here I could show overall all stats. just pick up the most importnat ones from pomodoro, habits, journal.... and put them here. Create different place for the weekly or monthly ones tho.	</h1>
+<h1>g Here I could show overall all stats. just pick up the most importnat ones from pomodoro, habits, journal.... and put them here. Create different place for the weekly or monthly ones tho.	</h1>
 
